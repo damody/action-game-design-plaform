f884d39c101315411eef640b4db1fb5f8d5710db
damody <t1238142000@gmail.com>
2012-07-17 08:02:24 -0700

    update: class InputState
    new finish:
    BackGround::CheckDataVaild
    BackGround::LoadData
    
    add:
    bg.lua

---
+++ trunk/ADGP/KTSG/bg.lua
@@ -0,0 +1,26 @@
+name="hello world"
+
+space_bounding =
+{
+	{xmin=0, ymin=0, zmin=0, xmax=2400, ymax=300, zmax=220},
+}
+
+ban_bounding =
+{
+	{xmin=50, ymin=50, zmin=50, xmax=100, ymax=100, zmax=100},
+}
+
+layer = 
+{
+	{picture_path="bg\sys\ft\sky.bmp", ground=0, x=50, y=50, z=50, w=100, h=100, loop_distance=0, timeline=1, time_start=0, time_end=0},
+}
+
+color_rect =
+{
+	{r=1.0, g=0, b=0, a=0.1, ground=0, x=50, y=50, z=50, w=100, h=100},
+}
+
+parallel_light = {
+	{x=50, y=50, z=50, light=100, timeline=1, time_start=0, time_end=0},
+}
+
