28b338ef70dbfba46f7c9453c78a2a951f20d1a9
DiscreteKat <kn810609@gmail.com>
2012-08-22 01:30:52 +0800

    fix KeyBoard 組合鑑問題 ("T"-昇龍霸)
    Signed-off-by: DiscreteKat <kn810609@gmail.com>

---
+++ trunk/ADGP/KTSG/game/CtrlKey.cpp
@@ -0,0 +1,58 @@
+#include "game/CtrlKey.h"
+
+#include "DX11\InputState.h"
+#include "global.h"
+
+KeyQueue Keyboard::Update()
+{
+	KeyQueue keyQue;
+	//InputState::GetInput();
+	if (InputStateS::instance().isKeyDown(KEY_UP))
+	{
+		std::cout<<"test \n";
+	}
+	for (CtrlKeyMap::iterator it_map = m_CtrlKeyMap.begin() ; it_map != m_CtrlKeyMap.end() ;it_map++)
+	{
+		if (it_map->second.size() ==1)
+		{
+			if (InputStateS::instance().isKeyDown(it_map->first))
+			{
+				KeyInfo info;
+				info.key = it_map->second.front();
+				info.time = g_Time;
+				keyQue.push_back(info);
+			}
+
+			if (InputStateS::instance().isKeyUp(it_map->first))
+			{
+				KeyInfo info;
+				info.key = (CtrlKey::v)((int)it_map->second.front()+KEYUP);
+				info.time = g_Time;
+				keyQue.push_back(info);
+			}
+		}else
+		{
+			if (InputStateS::instance().isKeyDown(it_map->first))
+			{
+				for (CtrlKeys::iterator it = it_map->second.begin(); it != it_map->second.end(); it++)
+				{
+					KeyInfo info;
+					info.key = (*it);
+					info.time = g_Time;
+					keyQue.push_back(info);
+
+					info.key = (CtrlKey::v)((int)(*it)+KEYUP);
+					info.time = g_Time;
+					keyQue.push_back(info);
+				}
+			}
+		}
+		
+	}
+	return keyQue;
+}
+
+void Keyboard::SetCtrlKey( int index,CtrlKeys keys )
+{
+	m_CtrlKeyMap[index] = keys;
+}
