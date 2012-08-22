a53cc9214d7ff153632c4ec7a60bdf46210c7a95
DiscreteKat <kn810609@gmail.com>
2012-08-22 22:50:16 +0800

    Add
    Signed-off-by: DiscreteKat <kn810609@gmail.com>

---
+++ trunk/ADGP Designer/ADGP Designer/global.cpp
@@ -0,0 +1,4 @@
+#include "global.h"
+
+ID3D11Device*	g_d3dDevice;
+TextureManager g_TextureManager;
---
+++ trunk/ADGP Designer/ADGP Designer/global.h
@@ -0,0 +1,7 @@
+#pragma once
+
+#include <d3d11.h>
+#include "DX11/TextureManager.h"
+
+extern ID3D11Device*  g_d3dDevice;
+extern TextureManager g_TextureManager;
