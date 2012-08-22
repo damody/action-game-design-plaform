adb7bfd14bad41f6200225e301f6b9ac47de509b
DiscreteKat <kn810609@gmail.com>
2012-08-22 22:48:53 +0800

    Add
    Signed-off-by: DiscreteKat <kn810609@gmail.com>

---
+++ trunk/ADGP Designer/ADGP Designer/DX11/TextureManager.cpp
@@ -0,0 +1,84 @@
+#include "StdGame.h"
+#include "TextureManager.h"
+#include "d3dApp.h"
+#include "global.h"
+TextureManager::TextureManager(void):m_index(-1)
+{
+}
+
+TextureManager::~TextureManager(void)
+{
+}
+
+int TextureManager::AddTexture( std::string path)
+{
+		int index=Find(path);
+		if (index != -1)
+		{
+			return index;
+		}
+		
+		m_Textures.push_back(Texture_Sptr(new Texture(path)));
+		m_List.push_back(path);
+		m_index++;
+		return m_index;
+}
+
+int TextureManager::AddTexture( std::string name , Texture_Sptr texture )
+{
+	m_Textures.push_back(texture);
+	m_List.push_back(name);
+	m_index++;
+	return m_index;
+}
+
+
+Texture_Sptr TextureManager::GetTexture(unsigned int index )
+{
+	if(index < m_Textures.size())
+				return m_Textures[index];
+	else return Texture_Sptr();
+}
+
+int TextureManager::Find( std::string path )
+{
+	int index=-1;
+	for(std::vector<std::string>::iterator it = m_List.begin();it != m_List.end(); it++)
+	{
+		index++;
+		if((*it)==path)return index;
+	}
+	return -1;// Not Found
+}
+
+Texture::Texture( std::string path )
+{
+	assert(g_d3dDevice!=0); // check init ok
+	FILE* file = fopen(path.c_str(), "r");
+	assert(file!=0); // check file ok
+	fclose(file);
+	if (g_d3dDevice)
+		D3DX11CreateShaderResourceViewFromFileA(g_d3dDevice, path.c_str(), 0, 0, &texture, 0);
+}
+
+Texture::Texture( std::wstring path )
+{
+	assert(g_d3dDevice!=0); // check init ok
+	FILE* file = _wfopen(path.c_str(), L"r");
+	assert(file!=0); // check file ok
+	fclose(file);
+	if (g_d3dDevice)
+		D3DX11CreateShaderResourceViewFromFileW(g_d3dDevice, path.c_str(), 0, 0, &texture, 0);
+}
+
+Texture::Texture( ID3D11ShaderResourceView* rc )
+{
+	texture = rc;
+}
+
+
+
+Texture::~Texture()
+{
+	ReleaseCOM(texture);
+}
---
+++ trunk/ADGP Designer/ADGP Designer/DX11/TextureManager.h
@@ -0,0 +1,56 @@
+#pragma once
+#include "StdGame.h"
+#include <vector>
+#include <map>
+#include <cassert>
+#include <cstdio>
+#include <windows.h>
+#include "common/shared_ptr.h"
+
+
+struct Texture
+{
+	Texture(std::string path);
+	Texture(std::wstring path);
+	Texture(ID3D11ShaderResourceView* rc);
+	~Texture();
+	ID3D11ShaderResourceView* texture;
+	operator ID3D11ShaderResourceView*()
+	{
+		return texture;
+	}
+	bool operator < (const Texture& rhs)
+	{
+		return texture < rhs.texture;
+	}
+	bool operator == (const Texture& rhs)
+	{
+		return texture == rhs.texture;
+	}
+};
+SHARE_PTR(Texture)
+
+typedef std::vector<Texture_Sptr> Textures;
+typedef std::map<int, Texture_Sptr> TextureMaps;
+
+class TextureManager
+{
+public:
+	TextureManager(void);
+	~TextureManager(void);
+	int AddTexture(std::string path);
+	int AddTexture(std::string name , Texture_Sptr texture);
+	int Find(std::string path);
+	Texture_Sptr GetTexture(unsigned int index);
+private:
+	Textures	m_Textures;
+	std::vector<std::string> m_List;
+	int		m_index;
+};
+
+struct DrawVertexGroup
+{
+	Texture_Sptr	texture;	// §÷½è
+	int	VertexCount, StartVertexLocation;
+};
+typedef std::vector<DrawVertexGroup> DrawVertexGroups;
