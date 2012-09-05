373989b78bd0c89d4224a6d93c91c33cea977564
damody <t1238142000@gmail.com>
2012-09-05 22:51:56 +0800

    remove adgp

--- trunk/ADGP/ShootingGameAlgorithmLibrary/src/ball/gravity.cpp
+++
@@ -1,9 +0,0 @@
-#include "gravity.h"
-
-int Gravity::UpdateBall( Ball* b, float elapsedtime )
-{
-	mForce -= mGravity * elapsedtime;
-	b->mPosition += b->mDirection * mVelocity * elapsedtime;
-	b->mPosition += mForce;
-	return Ball::FLY;
-}
