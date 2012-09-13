663347b729c4c32c8554f2f4befe87ade19987a8
brokenstar <f59107@gmail.com>
2012-09-13 14:01:15 +0800

    Polygon2D new member 	m_zRange 	m_zPoint new function 	zIsCollision 	SetZRange 	GetZRange 	GetZPoint fix 	Polygon2D 	Offset

--- trunk/SGAL/include/math/Polygon2D.h
+++ trunk/SGAL/include/math/Polygon2D.h
@@ -1,3 +1,4 @@
+#pragma warning(disable:4819)
 #pragma once
 
 #ifndef _POLYGON2D_H_
@@ -15,11 +16,10 @@
 typedef boost::geometry::model::d2::point_xy<float> point2;
 typedef boost::geometry::model::polygon<point2> polygon;
 
-class Polygon2D
+class Polygon2D 
 {
 public:
-	Polygon2D()
-		: m_angle( 0 ) {}
+	Polygon2D():m_angle(0), m_zPoint(0), m_zRange(0.01){}
 	Vec2s& Points()
 	{
 		m_needBuildEdges = true;
@@ -29,44 +29,44 @@ public:
 	{
 		return m_points;
 	}
-	void AddPoint( float x, float y );
-	void AddPoint( const Vec2& p );
-	void Offset( float x, float y );
-	void Offset( const Vec2& v );
-	void Offset( const Vec3& v );
-	void SetAngle( float angle );
-	float GetAngle()
-	{
-		return m_angle;
-	}
-	void Rotation( float angle, const Vec2& middle = Vec2::ZERO );
-	bool IsCollision( const Polygon2D& rhs );
+	void AddPoint(float x, float y);
+	void AddPoint(const Vec2& p);
+	void Offset(float x, float y);
+	void Offset(const Vec2& v);
+	void Offset(float x, float y, float z);
+	void Offset(const Vec3& v);
+	void SetAngle(float angle);
+	void SetZRange(float scale){m_zRange = scale;}
+	float GetAngle(){return m_angle;}
+	float GetZRange(){return m_zRange;}
+	float GetZPoint(){return m_zPoint;}
+	void Rotation(float angle, const Vec2& middle = Vec2::ZERO);
+	bool IsCollision(const Polygon2D& rhs);
+	bool zIsCollision(const Polygon2D& rhs);
 	void CheckBuildEdges();
 	void Clear();
 private:
 	void BuildEdges();
 	// Calculate the distance between [minA, maxA] and [minB, maxB]
 	// The distance will be negative if the intervals overlap
-	inline float IntervalDistance( float minA, float maxA, float minB, float maxB )
+	inline float IntervalDistance(float minA, float maxA, float minB, float maxB)
 	{
-		if ( minA < minB )
-		{
+		if (minA < minB) {
 			return minB - maxA;
-		}
-		else
-		{
+		} else {
 			return minA - maxB;
 		}
 	}
 	// Calculate the projection of a polygon on an axis and returns it as a [min, max] interval
-	void ProjectPolygon( const Vec2& axis, const Polygon2D& polygon, float* min, float* max );
+	void ProjectPolygon(const Vec2& axis, const Polygon2D& polygon, float* min, float* max);
 private:
 	float	m_angle;
 	Vec2s	m_points, m_edges;
 	bool	m_needBuildEdges;
 	polygon m_polygon;
-
+	float   m_zRange; // ª½®|
+	float   m_zPoint;
 };
+typedef std::vector<Polygon2D> Polygons;
 typedef std::vector<Polygon2D> Polygon2Ds;
-
 #endif // _POLYGON2D_H_
