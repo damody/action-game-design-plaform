8434d7907c0f9b812b2dacb2350ccea32b240a55
JingLin Wang <jojllman@gmail.com>
2012-09-26 11:57:03 +0800

    Fix
    Signed-off-by: JingLin Wang <jojllman@gmail.com>

---
+++ trunk/AGDP Designer/Edit/CommandLambda.cpp
@@ -0,0 +1,58 @@
+#include "CommandLambda.h"
+#include <exception>
+
+bool CommandLambda::Execute()
+{
+	try
+	{
+		for(std::size_t i=0;i<m_RedoFunctions.size();i++)
+		{
+			m_RedoFunctions[i]();
+		}
+
+		return true;
+	}
+	catch (std::exception& e)
+	{
+		std::cout << e.what() << std::endl;
+		return false;
+	}
+}
+
+bool CommandLambda::Unexecute()
+{
+	try
+	{
+		for(std::size_t i=0;i<m_UndoFunctions.size();i++)
+		{
+			m_UndoFunctions[i]();
+		}
+
+		return true;
+	}
+	catch (std::exception& e)
+	{
+		std::cout << e.what() << std::endl;
+		return false;
+	}
+}
+
+void CommandLambda::AddRedoFunction( std::function<void(void)> func )
+{
+	m_RedoFunctions.push_back(func);
+}
+
+void CommandLambda::ClearRedoFunctions()
+{
+	m_RedoFunctions.clear();
+}
+
+void CommandLambda::AddUndoFunction( std::function<void(void)> func )
+{
+	m_UndoFunctions.push_back(func);
+}
+
+void CommandLambda::ClearUndoFunctions()
+{
+	m_UndoFunctions.clear();
+}
