4667de0080b111ac762dd6f1d1adca55a6fd5d09
DiscreteKat <kn810609@gmail.com.tw>
2012-10-04 16:14:06 +0800

    Object.cpp(chee + weapon)
    Signed-off-by: DiscreteKat <kn810609@gmail.com.tw>

---
+++ trunk/AGDP/game/Object.cpp
@@ -0,0 +1,178 @@
+#include "Object.h"
+#include "global.h"
+
+Object::Object( void )
+{
+}
+
+Object::Object( std::wstring obj ):
+	m_ObjectName( obj ), m_Position( Vector3( 0, 0, 0 ) ), m_Vel( Vector3( 0, 0, 0 ) ), m_Team( 0 ), m_FaceSide( true ), m_FrameID( 0 ), m_Texture( 0 ), m_PicID( 0 ), m_PicW( 0 ), m_PicH( 0 ), m_PicX( 0 ), m_PicY( 0 )
+{
+	m_ObjectInfo = g_ObjectInfoManager.GetObjectInfo( m_ObjectName );
+
+	if ( m_ObjectInfo.get() )
+	{
+		this->Init();
+	}
+	else
+	{
+		std::wcout << L"Cannot find " << obj << std::endl;
+	}
+}
+
+
+Object::~Object( void )
+{
+}
+
+void Object::Init()
+{
+	m_Angle = 0;
+	m_HP = m_ObjectInfo->m_MaxHP;
+	m_Frame = L"default";
+	m_FrameID = 0;
+	FrameInfo* f = &m_ObjectInfo->m_FramesMap[m_Frame][m_FrameID];
+	m_PicID = f->m_PictureID;
+	m_PicX = f->m_PictureX;
+	m_PicY = f->m_PictureY;
+	m_PicW = m_ObjectInfo->m_PictureDatas[m_PicID].m_Column;
+	m_PicH = m_ObjectInfo->m_PictureDatas[m_PicID].m_Row;
+	m_Texture = m_ObjectInfo->m_PictureDatas[m_PicID].m_TextureID;
+	m_Action = f->m_HeroAction;
+	m_TimeTik = f->m_Wait;
+	m_Bodys = f->m_Bodys;
+	m_CenterX = f->m_CenterX;
+	m_CenterY = f->m_CenterY;
+}
+
+
+void Object::NextFrame()
+{
+	FrameInfo* f = &m_ObjectInfo->m_FramesMap[m_Frame][m_FrameID];
+	m_Frame = f->m_NextFrameName;
+	m_FrameID = f->m_NextFrameIndex;
+	f = &m_ObjectInfo->m_FramesMap[m_Frame][m_FrameID];
+	m_PicID = f->m_PictureID;
+	m_PicX = f->m_PictureX;
+	m_PicY = f->m_PictureY;
+	m_PicW = m_ObjectInfo->m_PictureDatas[m_PicID].m_Column;
+	m_PicH = m_ObjectInfo->m_PictureDatas[m_PicID].m_Row;
+	m_Texture = m_ObjectInfo->m_PictureDatas[m_PicID].m_TextureID;
+	m_Action = f->m_HeroAction;
+	m_TimeTik = f->m_Wait;
+	m_CenterX = f->m_CenterX;
+	m_CenterY = f->m_CenterY;
+
+	if ( m_FaceSide )
+	{
+		m_Vel.x += f->m_DVX;
+		m_Vel.y += f->m_DVY;
+		m_Vel.z += f->m_DVZ;
+	}
+	else
+	{
+		m_Vel.x -= f->m_DVX;
+		m_Vel.y -= f->m_DVY;
+		m_Vel.z -= f->m_DVZ;
+	}
+}
+
+void Object::Update( float dt )
+{
+	if ( m_TimeTik <= 0 )
+	{
+		NextFrame();
+	}
+	else
+	{
+		m_TimeTik--;
+	}
+
+	/*if (m_Vel.x < 0)
+	{
+		m_FaceSide = false;
+	}else{
+		m_FaceSide = true;
+	}//*/
+
+	/*
+	if ( g_BackgroundManager.GetCurrentBackground() != NULL )
+	{
+		if ( !g_BackgroundManager.GetCurrentBackground()->InSpace( m_Position ) )
+		{
+			g_ObjectManager.Destory( this, 6 );
+		}
+	}
+	*/
+	m_Position += m_Vel;
+}
+
+void Object::UpdateDataToDraw()
+{
+	float scale = 3.0f;
+	m_Pic.position.x = m_Position.x;
+	m_Pic.position.y = m_Position.y;
+	m_Pic.position.z = m_Position.z;
+	m_Pic.center.x = m_CenterX * scale;
+	m_Pic.center.y = m_CenterY * scale;
+	m_Pic.angle = m_Angle;
+	m_Pic.size.x = ( float )m_ObjectInfo->m_PictureDatas[m_PicID].m_Width * scale;
+	m_Pic.size.y = ( float )m_ObjectInfo->m_PictureDatas[m_PicID].m_Height * scale;
+	m_Pic.picpos.x = ( float )m_PicX;
+	m_Pic.picpos.y = ( float )m_PicY;
+	m_Pic.picpos.z = ( float )m_PicH;
+	m_Pic.picpos.w = ( float )m_PicW;
+	m_Pic.faceside = ( float )( m_FaceSide ? 1 : -1 );
+}
+
+void Object::SetTeam( int index )
+{
+	m_Team = index;
+}
+
+void Object::SetPosition( Vector3 p )
+{
+	m_Position = p;
+}
+
+void Object::Translation( Vector3 t )
+{
+	m_Position += t;
+}
+
+void Object::SetVelocity( Vector3 v )
+{
+	m_Vel = v;
+}
+
+int Object::GetTextureID()
+{
+	return m_Texture;
+}
+
+Texture_Sptr Object::GetTexture()
+{
+	return g_TextureManager.GetTexture( m_Texture );
+}
+
+ClipVertex Object::GetPic()
+{
+	return m_Pic;
+}
+
+Vector3 Object::Position()
+{
+	return m_Position;
+}
+
+ObjectType::e Object::ObjectType()
+{
+	return m_ObjectInfo->m_Type;
+}
+
+bool SortObject( Object_RawPtr a, Object_RawPtr b )
+{
+	return a->GetTextureID() < b->GetTextureID();
+}
+
+
---
+++ trunk/AGDP/game/Object.h
@@ -0,0 +1,71 @@
+#pragma once
+#include <string>
+#include "DX11\Vertex.h"
+#include "DX11\TextureManager.h"
+#include "math\Vector3.h"
+#include "common\shared_ptr.h"
+#include "game\ObjectInfo.h"
+#include "game\Record.h"
+
+class Object
+{
+private:
+	int				m_TimeTik;		//Count down TimeTik from Frame Wait
+	ClipVertex		m_Pic;
+	ObjectInfo_Sptr m_ObjectInfo;
+	int				m_Action;//當下動作狀態
+	std::wstring    m_Frame;//Current Frame
+	int				m_FrameID;//Current Frame ID
+
+	int				m_Texture;//Current Texture ID
+	int             m_PicID;
+	int				m_PicW;//W截切次數
+	int				m_PicH;//H截切次數
+	int				m_PicX;
+	int				m_PicY;
+
+	Vector3			m_Position;
+	float			m_CenterX, m_CenterY;
+	Vector3			m_Vel;
+	Bodys			m_Bodys;
+	float			m_Angle;
+	bool			m_FaceSide;//true 右, false 左
+
+	int				m_Team;//0為不分
+	int				m_HP;
+
+protected:
+	void Init();
+	void NextFrame();
+public:
+	const std::wstring m_ObjectName;
+
+public:
+	Object(void);
+	Object(std::wstring obj);
+	~Object(void);
+
+	void Update( float dt );
+	void UpdateDataToDraw();//Data To m_Pic
+
+	ObjectType::e ObjectType();
+
+	int Team(){return m_Team;}
+	void SetTeam( int index );
+
+	void SetPosition( Vector3 p );
+	void Translation( Vector3 t );
+	void SetVelocity( Vector3 v );
+
+	int GetTextureID();
+	Texture_Sptr GetTexture();
+	ClipVertex GetPic();
+	Vector3 Position();
+	friend bool Creat( const Vector3& pos, const Creation& obj, bool face, const Record_Sptr owner );
+};
+SHARE_PTR( Object );
+typedef std::vector <Object_RawPtr> Objects;
+
+bool SortObject( Object_RawPtr a, Object_RawPtr b );
+
+
