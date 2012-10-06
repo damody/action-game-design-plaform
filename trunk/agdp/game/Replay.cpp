08cfce9e492a32d73f250323b8778f390cbc5be3
minuteC <apple30467@gmail.com>
2012-10-06 18:21:44 +0800

    uncomplete.

--- trunk/agdp/game/Replay.cpp
+++ trunk/agdp/game/Replay.cpp
@@ -1,1 +1,28 @@
 #include "game/Replay.h"
+#include "ConvStr.h"
+#include <fstream>
+#include <boost/archive/binary_oarchive.hpp>
+#include <boost/archive/binary_iarchive.hpp>
+#include <boost/serialization/vector.hpp>
+#include <boost/serialization/string.hpp>
+
+void Replay::PushKeyInfo( KeyInfo ki )
+{
+	repalyKeyQueue.push_back( ki );
+}
+
+bool Replay::WriteToFile( std::wstring path )
+{
+	std::string _spath = ConvStr::GetStr( path );
+
+	std::ofstream ofs( _spath, std::ios::binary );
+
+	if ( !ofs.is_open() ) { return 0; }
+	{
+		//boost::archive::text_oarchive oa(ofs);
+		boost::archive::binary_oarchive oa( ofs );
+		oa << this->repalyKeyQueue;
+	}
+	ofs.close();
+	return 1;
+}
--- trunk/agdp/game/Replay.h
+++ trunk/agdp/game/Replay.h
@@ -1,12 +1,16 @@
 #pragma once
 #include <iostream>
+#include <vector>
 #include "game/CtrlKey.h"
+#include "global.h"
 
 namespace boost {namespace serialization {class access;}}
 
 class Replay
 {
 private:
+	std::vector< KeyInfo > repalyKeyQueue;
+
 	friend class boost::serialization::access;
 	template<class Archive>
 	void serialize( Archive& ar, const unsigned int version )
@@ -14,7 +18,8 @@ private:
 		ar&		repalyKeyQueue;
 	}
 public:
-	Replay();
-	~Replay();
-	std::vector< KeyInfo > repalyKeyQueue;
+	Replay() { repalyKeyQueue.clear(); }
+	~Replay() {}
+	void PushKeyInfo( KeyInfo ki );
+	bool WriteToFile( std::wstring path );
 };
