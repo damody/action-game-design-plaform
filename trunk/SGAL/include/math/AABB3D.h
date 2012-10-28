5bc60ee200d9b8e9ac9534ef202f3040e27b6ba1
JingLin Wang <jojllman@gmail.com>
2012-10-28 16:44:40 +0800

    Add AABB3D.
    Signed-off-by: JingLin Wang <jojllman@gmail.com>

---
+++ trunk/SGAL/include/math/AABB3D.h
@@ -0,0 +1,30 @@
+#pragma once
+#include "Vector2.h"
+#include "Vector3.h"
+#include "Polygon2D.h"
+
+class AABB3D
+{
+public:
+	AABB3D( const Polygon2D& poly );
+	void AddPoint( const Vector2& p );
+	AABB3D();
+	AABB3D( const Vector2& orgin );
+	AABB3D( const Vector3& orgin );
+	void ReBuild( const Polygon2D& poly );
+	void ReBuild( const Polygon2Ds& polys );
+	void AddPolygon2D( const Polygon2D& poly );
+	void AddPoint( const point2& p );
+	void Larger( float val );
+	void Larger( float x, float y, float z );
+	void SetBounding( float left, float right, float top, float down, float front, float back );
+	void SetBounding( float value );
+	bool IsContain( const AABB3D& rhs );
+	bool IsCollision( const AABB3D& rhs );
+	void Move(float x, float y, float z);
+	void Scale(float v);
+	void Scale( float x, float y, float z );
+	void ChangeFace();
+	Vector3 m_Min, m_Max, m_Len;
+};
+bool Collision( const AABB3D& lhs, const AABB3D& rhs );
